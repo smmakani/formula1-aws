b73f109910bbbac92e9f7e2c70c770d25c3464f5
smmakani@gmail.com <smmakani@gmail.com>
2025-04-13 15:03:51 +0000

    Commit All Files

---
+++ Formula1-Project-Solutions/Section-21/ingestion/9.create_processed_database.sql
@@ -0,0 +1,12 @@
+-- Databricks notebook source
+USE CATALOG home;
+USE SCHEMA formula1;
+
+-- COMMAND ----------
+
+CREATE TABLE IF NOT EXISTS f1_processed
+
+
+-- COMMAND ----------
+
+DESC HISTORY f1_processed
